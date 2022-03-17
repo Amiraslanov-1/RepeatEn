0180fcf347510dfcf79d3a98e041616c1266bc84
Amiraslanov-1 <99805882+Amiraslanov-1@users.noreply.github.com>
2022-03-17 04:37:13 +0400

    Task 17.03.2022 added

---
+++ ClassGroup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class ClassGroup
+    {
+        private string _no;
+        private int _studentLimit;
+        public string Name;
+        public string surname;
+        public int age;
+
+        public ClassGroup(string name, string surname, int age)
+        {
+            this.Name = name;   
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public string GetInfo()
+        {
+            return $"Student Name : {this.Name}\nStudent Surname : {this.surname}\nStudent Age : {this.age}\n" ;
+        }
+
+        public string No
+        {
+            get => this._no;
+            set
+            {
+                if( CheckValidation(value))
+                {
+                    this._no = value;
+                }
+               
+            }
+        }
+        public int StudentLimit
+        {
+            get => _studentLimit;
+            set
+            {
+                if (value >= 0)
+                {
+                    this._studentLimit = value;
+                }
+            }
+        }
+
+        static bool CheckValidation(string name)
+        {
+
+            if (!string.IsNullOrWhiteSpace(name)&& name.Length < 6 && name.Length>4)
+            {   
+                
+                if (char.IsUpper(name[0]) && char.IsUpper(name[1]) && char.IsDigit(name[2]) && char.IsDigit(name[3]) && char.IsDigit(name[4]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Length;
+            string LengthStr;
+            Console.WriteLine("Enter the number of Student :");
+            LengthStr = Console.ReadLine();
+            Console.WriteLine("------------------------------------|");
+
+            while (CheckString(LengthStr) !=true )
+            {
+                Console.WriteLine("Enter Correctly :");
+                LengthStr = Console.ReadLine();
+                Console.WriteLine("------------------------------------|");
+
+            }
+            Length = Convert.ToInt32(LengthStr);
+
+            string GroupName;
+            do
+            {
+                Console.WriteLine("Enter Group Name :");
+                GroupName = Console.ReadLine();
+                Console.WriteLine("------------------------------------|");
+
+            } while (!CheckValidation(GroupName));
+
+
+            ClassGroup[] Group = new ClassGroup[Length];
+
+            for (int i = 0; i < Length; i++)
+            {
+       
+                string Name = GetStudentName ($"{i + 1} - Student Name : ", 0, 20);
+                string Surname = GetStudentSurname($"{i + 1} - Student  Surname :", 0, 20);
+                int Age = GetStudentAge($"{i + 1} - StudentAge", 17,45);
+
+                Group[i] = new ClassGroup(Name, Surname, Age);
+       
+            }
+            for (int i = 0; i < Group.Length; i++)
+            {
+                Console.WriteLine();
+              
+                Console.WriteLine($"|------------------------- {i + 1}st Student from {GroupName} Group -------------------------| \n\n");
+                Console.WriteLine(Group[i].GetInfo());
+            }
+        }
+
+
+
+
+
+        static string GetStudentName(string name, int min, int max)
+        {
+
+            string Input;
+            do
+            {
+                Console.WriteLine(name);
+                Input = Console.ReadLine();
+                Console.WriteLine("------------------------------------|");
+
+            } while (Input.Length < min || max < Input.Length);
+
+            return Input;
+        }
+
+
+
+        static string GetStudentSurname(string name, int min, int max)
+        {
+
+            string Input;
+            do
+            {
+                Console.WriteLine(name);
+                Input = Console.ReadLine();
+                Console.WriteLine("------------------------------------|");
+
+            } while (Input.Length < min || max < Input.Length);
+
+            return Input;
+        }
+
+
+        static int GetStudentAge(string name, int min, int max)
+        {
+            int Input;
+            string inputStr;
+
+            do
+            {
+                Console.WriteLine(name);
+                inputStr = Console.ReadLine();
+                Input = Convert.ToInt32(inputStr);
+                Console.WriteLine("------------------------------------|");
+
+
+            } while (Input < min || max < Input);
+
+            return Input;
+        }
+
+
+
+
+        static bool CheckValidation(string name)
+        {
+
+            if (!string.IsNullOrWhiteSpace(name) && name.Length < 6 && name.Length > 4)
+            {
+
+                if (char.IsUpper(name[0]) && char.IsUpper(name[1]) && char.IsDigit(name[2]) && char.IsDigit(name[3]) && char.IsDigit(name[4]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+        static bool CheckString(string count)
+        {
+
+            if (!string.IsNullOrWhiteSpace(count) )
+            {
+
+                if (count.Length > 0)
+                {
+                    return true;
+                }
+               
+
+            }
+            return false;
+
+        }
+
+
+
+
+
+
+    }
+    }
+
+
